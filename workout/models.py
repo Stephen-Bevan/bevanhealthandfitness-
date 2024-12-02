45a00f72734bfa2d2a08038e5e12f41320f3a854
Stephen Bevan <mrsteverox@gmail.com>
2024-12-02 13:11:28 +0000

    added workout model

--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workouts')
+    day = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.day}"
+
+
+class Exercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='exercises')
+    name = models.CharField(max_length=100)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weights = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.sets} sets, {self.reps} reps, {self.weights} kg)"
--- workout/urls.py
+++ workout/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # Fixed URL and name
     path('my-login/', views.my_login, name='my-login'),
     path('user-logout/', views.user_logout, name='user-logout'),
+    
 
 
 
